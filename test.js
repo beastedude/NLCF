8b36ebae439d00862ce54c616b7d6e67f4ba4a51
beastedude <beastedude@gmail.com>
2013-10-08 07:01:30 -0400

    Create test.js

---
+++ test.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+  $(".slider #1").show("fade,500");
+  $(".slider #1").delay(10000).hide(1000);
+  $(".slider #2").show(1000);
+  $(".slider #2").delay(20000).hide(1000);
+  $(".slider #3").show(1000)
+  $(".slider #3").delay(30000).hide(1000);
+});
